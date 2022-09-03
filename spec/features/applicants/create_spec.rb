4e7562b45c244e83d77abb50c08da2c8220f33c3
Lucas Colwell <colwellc37@gmail.com>
2022-09-03 17:55:12 -0600

    Test: update for story 4

--- spec/features/applicants/create_spec.rb
+++ spec/features/applicants/create_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'application creation' do
-  describe 'the application new' do
+  describe 'The new application page' do
     it "renders the new form" do
       visit '/applicants/new'
 
@@ -17,49 +17,61 @@ RSpec.describe 'application creation' do
     end
   end
 
-  describe 'the application create' do
-    context 'given valid data' do
-      it 'creates the application' do
-        visit '/applicants/new'
 
-        fill_in 'First name', with: 'Linda'
-        fill_in 'Last name', with: 'Smith'
-        fill_in 'Street address', with: '737 Royal Lane'
-        fill_in 'City', with: 'Columbus'
-        select 'OH', :from => 'state'
-        fill_in 'Zipcode', with: '39053'
-        fill_in 'Description', with: 'I would make a great pet owner'
-        fill_in 'Status', with: 'In Progress'
-        click_button 'Create Application'
+  context 'given valid data' do
+    it 'creates the application' do
+      visit '/applicants/new'
+
+      fill_in 'First name', with: 'Linda'
+      fill_in 'Last name', with: 'Smith'
+      fill_in 'Street address', with: '737 Royal Lane'
+      fill_in 'City', with: 'Columbus'
+      select 'OH', :from => 'state'
+      fill_in 'Zipcode', with: '39053'
+      fill_in 'Description', with: 'I would make a great pet owner'
+      fill_in 'Status', with: 'In Progress'
+      click_button 'Create Application'
 
 
-        # Then I am taken to the new application's show page
-        expect(page).to have_content('Linda')
-        expect(page).to have_content('Smith')
-        expect(page).to have_content('737 Royal Lane')
-        expect(page).to have_content('Columbus')
-        expect(page).to have_content('OH')
-        expect(page).to have_content('I would make a great pet owner')
-        expect(page).to have_content('In Progress')
+      # Then I am taken to the new application's show page
+      expect(page).to have_content('Linda')
+      expect(page).to have_content('Smith')
+      expect(page).to have_content('737 Royal Lane')
+      expect(page).to have_content('Columbus')
+      expect(page).to have_content('OH')
+      expect(page).to have_content('I would make a great pet owner')
+      expect(page).to have_content('In Progress')
 
-        expect(page).not_to have_content('Sherry')
+      expect(page).not_to have_content('Sherry')
+    end
+  end
+
+  context 'If I submit without filling in any of the form fields' do
+    it "Then I am taken back to the new applications page" do
+      visit '/applicants/new'
+
+      fill_in 'First name', with: 'Linda'
+      fill_in 'Last name', with: 'Smith'
+      fill_in 'Street address', with: '737 Royal Lane'
+      fill_in 'City', with: 'Columbus'
+      select 'OH', :from => 'state'
+      #zipcode was purposely not filled
+      fill_in 'Description', with: 'I would make a great pet owner'
+      fill_in 'Status', with: 'In Progress'
+      click_button 'Create Application'
+
+      expect(current_path).to eq('/applicants/new')
+
+      it "And I see a message that I must fill in those fields." do
+        expect(page).to have_content("All forms must be filled")
       end
     end
   end
 end
-    # As a visitor
-    # XX When I visit the pet index page
-    # XX Then I see a link to "Start an Application"
-    # XX When I click this link
-    # XX Then I am taken to the new application page where I see a form
-    # XX When I fill in this form with my:
-    #
-    # Name
-    # Street Address
-    # City
-    # State
-    # Zip Code
-    # And I click submit
-    # ***Then I am taken to the new application's show page
-    # And I see my Name, address information, and description of why I would make a good home
-    # And I see an indicator that this application is "In Progress"
+# As a visitor
+# When I visit the new application page
+# And I fail to fill in any of the form fields
+# And I click submit
+# Then I am taken back to the new applications page
+# And I see a message that I must fill in those fields.
+# bonus - keep the info filled in instead of clearing all fields???
